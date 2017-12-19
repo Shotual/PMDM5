2698ac657352a8f0235925483617be08354c34b3
Guillermo Tuvilla Gómez <guillermo.tuvilla@live.u-tad.com>
2017-12-19 13:21:42 +0100

    Solucion de errores faltaba importar en graddles de appexamen la implemetacion de recycler a continuacion seguiremos con el second activituy ya para finalizar haremos el metodo que recoja la informacion de firebase como un collection y lo tranformaremos a objeto

---
+++ appexamen/src/main/java/com/example/appexamen/Adapters/ListaNoticiasAdapter.java
@@ -0,0 +1,78 @@
+package com.example.appexamen.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.appexamen.FBObjetos.FBNoticia;
+import com.example.appexamen.R;
+
+import java.util.ArrayList;
+import android.support.v7.widget.RecyclerView;
+/**
+ * Created by guille on 19/12/17.
+ */
+
+public class ListaNoticiasAdapter extends RecyclerView.Adapter<NoticiaViewHolder> {
+
+    private ArrayList<FBNoticia> noticias;
+    private Context mContext;
+
+    public ListaNoticiasAdapter(ArrayList<FBNoticia> noticias, Context mContext){
+
+        this.noticias=noticias;
+        this.mContext=mContext;
+    }
+
+
+    @Override
+    public NoticiaViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.celda_noticia_layout,null);
+        NoticiaViewHolder noticiaViewHolder= new NoticiaViewHolder(view);
+        return noticiaViewHolder;
+    }
+
+    //DONDE SE VINCULA ENTRE LOS CONTENIDOS Y EL CONTENEDOR(CELDAVIEWHOLDER);
+
+    @Override
+    public void onBindViewHolder(NoticiaViewHolder holder, int position) {
+        // holder.textomensaje.setText(coches.get(position).original);
+
+        //IMPORTANTE ME COGE FABRICADO POR QUE SE CREE QUE ES UN ID
+
+        holder.tvnombre.setText(noticias.get(position).Nombre);
+        holder.tvjuego.setText(noticias.get(position).Juego);
+
+
+        Glide.with(mContext).load(noticias.get(position).imgurl)
+             .into(holder.imgnoticia);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return noticias.size();
+    }
+}
+
+//MOLDE
+class NoticiaViewHolder extends RecyclerView.ViewHolder{
+
+
+    public TextView tvjuego;
+    public TextView tvnombre;
+    public ImageView imgnoticia;
+
+
+    public NoticiaViewHolder(View itemView) {
+        super(itemView);
+
+        tvnombre=itemView.findViewById(R.id.tvnombre);
+        tvjuego=itemView.findViewById(R.id.tvjuego);
+        imgnoticia=itemView.findViewById(R.id.imgnoticias);
+    }
+}

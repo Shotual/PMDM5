32e1f027a777d27ad00d641ecbc99d39d216228f
Guillermo Tuvilla Gómez <guillermo.tuvilla@live.u-tad.com>
2017-12-19 13:26:00 +0100

    Descarga de noticias completada app apuntito del 10

--- appexamen/src/main/java/com/example/appexamen/SecondActivity.java
+++ appexamen/src/main/java/com/example/appexamen/SecondActivity.java
@@ -5,8 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 
+import com.example.appexamen.Adapters.ListaNoticiasAdapter;
+import com.example.appexamen.FBObjetos.FBNoticia;
 import com.example.milib.ListaFragment;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.GenericTypeIndicator;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Created by guille on 19/12/17.
@@ -28,7 +34,7 @@ public class SecondActivity extends AppCompatActivity {
 
         //PASAMOS POR PARAMETRO EL NOMBRE DE LA RAMA QUE QUIERAS EN LA BASE DE DATOS DE FIREBASE
         DataHolder.instance.fireBaseAdmin.descargarYObservarRama("Noticias");
-        
+
 
         Log.v("SecondActivity","--------EMAAAIL: "+DataHolder.instance.fireBaseAdmin.user.getEmail());
 
@@ -64,6 +70,18 @@ class SecondActivityEvents implements FireBaseAdminListener{
 
     @Override
     public void firebaseAdmin_ramaDescargada(String rama, DataSnapshot dataSnapshot) {
+        Log.v("RAMA",rama+"--------RAMA DESCARGADA............"+dataSnapshot);
 
+        if(rama.equals("Noticias")){
+
+            GenericTypeIndicator<ArrayList<FBNoticia>> indicator=new GenericTypeIndicator<ArrayList<FBNoticia>>(){};
+            ArrayList<FBNoticia> noticias=dataSnapshot.getValue(indicator);
+            //VALUES NO ES UN ARRAY LIST ES UN COLLECTIONS
+            Log.v("noticias","noticias CONTIENE: "+noticias);
+
+            //PARA TRANSFORMAR UN COLLECTION A UN ARRAY LIST HAY QUE HACER es new ArrayList<Mensaje>(msg.values())
+            ListaNoticiasAdapter listaNoticiasAdapter=new ListaNoticiasAdapter(noticias,secondActivity);
+            secondActivity.listaFragmentNoticias.recyclerView.setAdapter(listaNoticiasAdapter);
+        }
     }
 }

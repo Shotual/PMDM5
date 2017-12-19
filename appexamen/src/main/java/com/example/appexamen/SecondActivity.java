29c8cd02c7bdf52d21ed3799fd1c3da566b052ef
Guillermo Tuvilla Gómez <guillermo.tuvilla@live.u-tad.com>
2017-12-19 13:15:54 +0100

    Errores varios recycler y secondActivity clase rellena con metodos

--- appexamen/src/main/java/com/example/appexamen/SecondActivity.java
+++ appexamen/src/main/java/com/example/appexamen/SecondActivity.java
@@ -1,7 +1,11 @@
 package com.example.appexamen;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+
+import com.example.milib.ListaFragment;
 import com.google.firebase.database.DataSnapshot;
 
 /**
@@ -9,6 +13,33 @@ import com.google.firebase.database.DataSnapshot;
  */
 
 public class SecondActivity extends AppCompatActivity {
+  ListaFragment listaFragmentNoticias;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        SecondActivityEvents events=new SecondActivityEvents(this);
+        DataHolder.instance.fireBaseAdmin.setListener(events);
+
+        listaFragmentNoticias =(ListaFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentListNoticias);
+
+
+        //PASAMOS POR PARAMETRO EL NOMBRE DE LA RAMA QUE QUIERAS EN LA BASE DE DATOS DE FIREBASE
+        DataHolder.instance.fireBaseAdmin.descargarYObservarRama("Noticias");
+        
+
+        Log.v("SecondActivity","--------EMAAAIL: "+DataHolder.instance.fireBaseAdmin.user.getEmail());
+
+
+      /*  ArrayList<String> mdatos= new ArrayList<>();
+        mdatos.add("MENSAJE1");
+        mdatos.add("MENSAJE2");
+
+        ListaMensajesAdapter listaMensajesAdapter = new ListaMensajesAdapter(mdatos);
+        listaFragment.recyclerView.setAdapter(listaMensajesAdapter); */
+    }
 
 }
 class SecondActivityEvents implements FireBaseAdminListener{

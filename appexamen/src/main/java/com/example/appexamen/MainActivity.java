c7218b5e95f44db7d2343366798f1441ccaf2ea3
Guillermo Tuvilla Gómez <guillermo.tuvilla@live.u-tad.com>
2017-12-19 11:24:02 +0100

    creacion de clase mainactivity eventes implementando loginfragment y registrofragment :))))))

--- appexamen/src/main/java/com/example/appexamen/MainActivity.java
+++ appexamen/src/main/java/com/example/appexamen/MainActivity.java
@@ -3,11 +3,39 @@ package com.example.appexamen;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.milib.LoginFragmentListener;
+import com.example.milib.RegisterFragmentListener;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+}
+
+
+class MainActivityEvents implements LoginFragmentListener, RegisterFragmentListener {
+    MainActivity mainActivity;
+
+    @Override
+    public void registerFragmentRegisterButtonAceptarClicked(String sUser, String sPass) {
+
+    }
+
+    @Override
+    public void registerFragmentRegisterButtonCancelarClicked() {
+
+    }
+
+    @Override
+    public void loginFragmentLoginButtonClicked(String sUser, String sPass) {
+
+    }
+
+    @Override
+    public void loginFragmentRegisterButtonClicked() {
+
     }
 }

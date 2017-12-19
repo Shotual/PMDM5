0e9fa8acc3d81b58ef29661a399f772e65cd17b2
Guillermo Tuvilla Gómez <guillermo.tuvilla@live.u-tad.com>
2017-12-19 11:41:41 +0100

    INICIALIZACION DE MAIN ACTIVITY Y LISTENERS PARA QUE EL MAINACTIVITY ESCUCHE DEL LOGINFRAGMENT,REGISTER Y MAS ADELANTE DEL FIREBASE

--- appexamen/src/main/java/com/example/appexamen/MainActivity.java
+++ appexamen/src/main/java/com/example/appexamen/MainActivity.java
@@ -1,23 +1,40 @@
 package com.example.appexamen;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.milib.LoginFragment;
 import com.example.milib.LoginFragmentListener;
+import com.example.milib.RegisterFragment;
 import com.example.milib.RegisterFragmentListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    LoginFragment loginFragment;
+    RegisterFragment registerFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginFragment=(LoginFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentLogin );
+
+        MainActivityEvents mainActivityEvents= new MainActivityEvents(this);
+        loginFragment.setListener(mainActivityEvents);
+
+
     }
 }
 
 
 class MainActivityEvents implements LoginFragmentListener, RegisterFragmentListener {
     MainActivity mainActivity;
+
+    public MainActivityEvents(MainActivity mainActivity){
+        this.mainActivity=mainActivity;
+    }
 
     @Override
     public void registerFragmentRegisterButtonAceptarClicked(String sUser, String sPass) {

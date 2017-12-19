528eefa3dd6c5d5cd4bcb30916710acf21fe84fe
Guillermo Tuvilla Gómez <guillermo.tuvilla@live.u-tad.com>
2017-12-19 13:01:15 +0100

    Completada clase para el objeto noticias

--- appexamen/src/main/java/com/example/appexamen/FBObjetos/FBNoticia.java
+++ appexamen/src/main/java/com/example/appexamen/FBObjetos/FBNoticia.java
@@ -1,8 +1,25 @@
 package com.example.appexamen.FBObjetos;
 
+import com.google.firebase.database.IgnoreExtraProperties;
+
 /**
  * Created by guille on 19/12/17.
  */
-
+@IgnoreExtraProperties
 public class FBNoticia {
+
+    public String Juego;
+    public String Nombre;
+    public String imgurl;
+
+    public FBNoticia(){
+
+    }
+
+    public FBNoticia(String Juego,String Nombre, String imgurl){
+        this.Juego=Juego;
+        this.Nombre=Nombre;
+        this.imgurl=imgurl;
+
+    }
 }
